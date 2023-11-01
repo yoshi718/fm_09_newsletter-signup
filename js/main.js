430ef31bd4e85b4bad7390ccd749a2e3eb3c0d87
yoshi718 <133894392+yoshi718@users.noreply.github.com>
2023-11-01 11:59:15 -0400

    Initial Upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("form");
+const closeModalButton = document.querySelector("[data-close-button]");
+
+closeModalButton.addEventListener("click", () => {
+  window.location.href = "/";
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email");
+
+  const isEmail = /\S+@\S+.\S+/;
+
+  if (!isEmail.test(email.value)) {
+    const errorMsg = document.querySelector(".signup-container__form__error");
+
+    email.classList.add("input_invalid");
+    errorMsg.classList.add("show");
+
+    const old = email.value;
+    email.value = ``;
+    email.value = old;
+    email.focus();
+    return;
+  }
+
+  const enteredEmail = document.getElementById("enteredEmail");
+  enteredEmail.innerText = email.value;
+
+  const myModal = document.getElementById("modal");
+  const myOverlay = document.getElementById("overlay");
+
+  myModal.classList.add("active");
+  myOverlay.classList.add("active");
+});
